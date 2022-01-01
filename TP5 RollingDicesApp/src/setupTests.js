e2fbbd09654374fb739393469b7df8970b221d86
MayaFDev <93150212+MayaFDev@users.noreply.github.com>
2022-01-01 17:31:47 +0100

    Add files via upload

---
+++ TP5 RollingDicesApp/src/setupTests.js
@@ -0,0 +1,21 @@
+import React , {Compenent} from 'react'; 
+
+class test extends Compenent {
+     
+    render() {
+
+        return (
+
+            <div> 
+                <h1>Game c fait </h1>
+                <h2> h2 </h2>
+            </div>
+
+        )
+
+    }
+
+
+}
+
+export default test
