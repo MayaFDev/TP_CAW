54d6ef5c7937abaad119e2a40f81bc39b3af920d
MayaFDev <93150212+MayaFDev@users.noreply.github.com>
2022-01-01 17:28:06 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import LanceDe from './LanceDe';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1 >  ROLLING DICES APPLICATION    </h1>
+      <h2>  🎲🎲🎲 🎲🎲🎲 🎲🎲🎲 🎲🎲🎲 🎲🎲🎲</h2>
+     
+      <LanceDe /> 
+     
+    </div>
+  );
+}
+
+export default App;
